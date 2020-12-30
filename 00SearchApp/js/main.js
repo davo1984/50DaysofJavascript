107a38aaa942bbe9efc7d8b03cac13323b2c481c
David Oliver <davooliver1964@gmail.com>
2020-12-30 10:42:16 -0500

    Styled & some JS

---
+++ 00SearchApp/js/main.js
@@ -0,0 +1,36 @@
+import { setSearchFocus, showClearTextButton } from "./searchBar.js"
+import { deleteSearchResults, buildSearchResults, setStatsLine, clearStatsLine } from "./searchResults.js"
+import { getSearchTerm, retrieveSearchResults } from "./dataFunctions.js"
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+})
+
+const initApp = () => {
+    setSearchFocus()
+        // TODO: 3 listeners clear text
+    const search = document.getElementById("search")
+    search.addEventListener("input", showClearTextButton)
+
+    const form = document.getElementById("searchBar");
+    form.addEventListener("submit", submitTheSearch);
+}
+
+const submitTheSearch = (event) => {
+    event.preventDefault();
+    deleteSearchResults()
+    processTheSearch();
+    setSearchFocus()
+}
+
+//procedural
+const processTheSearch = async() => {
+    clearStatsLine()
+    const searchTerm = getSearchTerm();
+    if (searchTerm === "") return
+    const resultArray = await retrieveSearchResults(searchTerm)
+    if (resultArray.length) buildSearchResults(resultArray)
+    setStatsLine(resultArray.length)
+}

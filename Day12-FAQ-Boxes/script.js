623177d56ac402d466c1ae60d569fd19e5f07f53
David Oliver <davooliver1964@gmail.com>
2021-01-05 19:58:13 -0500

    started QnA cards

---
+++ Day12-FAQ-Boxes/script.js
@@ -0,0 +1,4 @@
+// bring in toggle buttons class=faq-toggle querySelectorAll
+// loop through nodelist  using forEach
+// add click event addEventListener
+// toggle the active class on the parent node .parentNode & classList.toggle()
